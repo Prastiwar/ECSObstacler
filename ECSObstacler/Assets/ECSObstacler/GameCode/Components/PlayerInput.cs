c8fc0ef9c1a59617872e16c494fe752fe4e40e64
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2018-04-28 11:25:03 +0200

    Initialize project

---
+++ ECSObstacler/Assets/ECSObstacler/GameCode/Components/PlayerInput.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct PlayerInput : IComponentData
+{
+    public float2 Move;
+    public bool1 IsGrounded;
+}
---
+++ ECSObstacler/Assets/ECSObstacler/GameCode/Systems/PlayerInputSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+public class PlayerInputSystem : ComponentSystem
+{
+    struct PlayerInputData
+    {
+        public int Length;
+        public ComponentDataArray<PlayerInput> input;
+    }
+    [Inject] PlayerInputData inputData;
+
+    protected override void OnUpdate()
+    {
+        for (int i = 0; i < inputData.Length; i++)
+        {
+            var input = inputData.input[i];
+
+            input.Move.x = Input.GetAxis("Horizontal");
+            input.Move.y = Input.GetAxis("Vertical");
+
+            inputData.input[i] = input;
+        }
+    }
+}
